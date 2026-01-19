a90c5afe0e62751588e1749bfce8708f87671d71
tolotra1209 <150174630+tolotra1209@users.noreply.github.com>
2026-01-19 21:20:03 +0100

    Update minion.py

--- minion.py
+++ minion.py
@@ -6,20 +6,24 @@ def main():
     QueueManager.register("get_task_queue")
     QueueManager.register("get_result_queue")
 
-    m = QueueManager(address=("localhost", 50000), authkey=b"abc")
-    m.connect()
+    try:
+        m = QueueManager(address=("localhost", 50000), authkey=b"abc")
+        m.connect()
 
-    tq = m.get_task_queue()
-    rq = m.get_result_queue()
+        tq = m.get_task_queue()
+        rq = m.get_result_queue()
 
-    while True:
-        data = tq.get()
-        if data is None:
-            break
+        while True:
+            data = tq.get()
+            if data is None:
+                break
 
-        task = Task.from_json(data)
-        task.work()
-        rq.put(task.to_json())
+            task = Task.from_json(data)
+            task.work()
+            rq.put(task.to_json())
+    except Exception as e:
+        print(f"Erreur dans le minion: {e}")
+        raise
 
 
 if __name__ == "__main__":
